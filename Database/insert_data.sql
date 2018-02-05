ecec5ed1fc390d1b8b6802f8f2b9727bf197ae39
Ekaterina Kurysheva <kurysheva.katerina@gmail.com>
2018-02-05 22:08:30 +0100

    added photos table

--- Database/insert_data.sql
+++ Database/insert_data.sql
@@ -1,5 +1,8 @@
-exec add_user_credentials('admin', 'admin');
-exec ADD_USER_TOKEN('admin', 'token');
+exec ADD_USER_CREDENTIALS('admin', 'admin');
 
 exec ADD_NEW_RPI('rpi');
 exec CHANGE_RPI_STATE('rpi', 'ACTIVE');
+
+exec ADD_RPI_USER_RELATION('admin','rpi');
+
+COMMIT;
